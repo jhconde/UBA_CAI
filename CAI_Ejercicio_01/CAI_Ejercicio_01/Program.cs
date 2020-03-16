a73360aede377f7f16786ac52ea9952b1e734441
Jose Conde <jhconde@hotmail.com>
2020-03-15 23:00:07 -0300

    ejercicio 01

---
+++ CAI_Ejercicio_01/CAI_Ejercicio_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CAI_Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese texto: ");
+            char[] param = Console.ReadLine().ToCharArray();
+            int inicioIdx = 0, finIdx = param.Length - 1;
+            bool ok = true;
+            while (inicioIdx < finIdx)
+            {
+                if (param[inicioIdx] != param[finIdx])
+                {
+                    ok = false;
+                    break;
+                }
+                inicioIdx++;
+                finIdx--;
+            }
+            Console.WriteLine(ok ? "es palindromo" : "no es palindormo");
+        }
+    }
+}
