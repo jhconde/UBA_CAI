506f2b24ea4c543dea00b4b50eed77ab9bb74537
Jose Conde <jhconde@hotmail.com>
2020-03-16 21:30:28 -0300

    ejercicio 05

---
+++ CAI_Ejercicio_05/CAI_Ejercicio_05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CAI_Ejercicio_05
+{
+    class Program
+    {
+
+        // Crear una aplicación que, cuando el usuario ingresa un número de 5 cifras, lo invierta y muestre por pantalla 
+        // “El número invertido es: *nnnnn*”. Por ejemplo, si el usuario ingresa “12345”, mostrará por pantalla “54321”.
+        static void Main(string[] args) {
+            string entrada = "";
+            do {
+                Console.Write("Ingrese un numero de 5 cifras: ");
+                entrada = Console.ReadLine();
+            } while (entrada.Length < 5);
+            
+            for (int i = entrada.Length - 1; i > -1; i--) {
+                Console.Write(entrada[i]);
+            }
+        }
+    }
+}
