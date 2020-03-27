c02431d4ef3c6feff7789319a71f36f6f16316f1
Jose Conde <jhconde@hotmail.com>
2020-03-27 20:58:58 -0300

    agregando clase restaurante

---
+++ 00_Restaurante/00_Restaurante/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Restaurante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaurante rodizio = new Restaurante();
+            rodizio.setNombre("Rodizio");
+            rodizio.atenderMesa(1);
+            rodizio.atenderMesa(2);
+            rodizio.facturar(2, 100);
+            rodizio.facturar(1, 50);
+
+            Console.WriteLine("Facturacion de " + rodizio.getNombre() + ": " + rodizio.getCapital());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 00_Restaurante/00_Restaurante/Restaurante.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Restaurante
+{
+    class Restaurante
+    {
+        string nombre;
+        string especialidad;
+        string sucursal;
+        float capital = 0;
+
+        public void facturar(int numeroMesa, float importe)
+        {
+            Console.WriteLine("Facturando a la mesa " + numeroMesa + " $" + importe);
+            capital += importe; // aca el comporamiento cambia estado
+        }
+
+        public void atenderMesa(int numeroMesa)
+        {
+            Console.WriteLine("Atendiendo mesa " + numeroMesa);
+            if (especialidad == "pasta") { // aca el valor del estado decide que accion tomar
+                servirPlato("hondo");
+            } else {
+                servirPlato("comun");
+            }
+            Console.WriteLine("Mesa atendida " + numeroMesa);
+        }
+
+        private void servirPlato(string tipo)
+        {
+            Console.WriteLine("Sirviendo plato " + tipo);
+        }
+        
+        public float getCapital()
+        {
+            return capital;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public void setNombre(string nombreParametro)
+        {
+            nombre = nombreParametro;
+        }
+
+    }
+}
