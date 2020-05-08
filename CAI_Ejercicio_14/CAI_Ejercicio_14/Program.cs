4d22550ae07a1d28958f62052512631e1035ca38
Jose Conde <jhconde@hotmail.com>
2020-05-08 19:34:45 -0300

    Ejercicio 14

---
+++ CAI_Ejercicio_14/CAI_Ejercicio_14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Ejercicio_14
+{
+    class Program
+    {
+        // Crear una aplicación que, cuando el usuario ingrese un número, muestre su tabla de multiplicar por pantalla.
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            int numero = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " x " + i + " = " + numero * i);
+            }
+            Console.ReadLine(); // mantiene la consola abierta
+        }
+    }
+}
